cf102b2e9a84ef9e61e38d1b8c1bc611258d2aa0
Ben Peterson <ben@mlsmetro.com>
2020-11-06 16:15:34 -0500

    Update PSNTFS.psm1

--- PSNTFS.psm1
+++ PSNTFS.psm1
@@ -1,5 +1,31 @@
-#Disable-Inheritanse
-#Set-UserReadPermission
-#Set-UserModifyPermission
-#Set-UserFullControlPermission
-#Set-UserAsOwner
+#Add-UserReadPermission
+#Add-UserModifyPermission
+#Add-UserFullControlPermission
+#Add-UserAsOwner
+#Remove-UserReadPermission
+#Remove-UserModifyPermission
+#Remove-UserFullControlPermission
+#Get-UserPermission
+
+function Disable-FolderInheritance{
+    <#
+    #>
+
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true)]
+        [string]$directoryPath
+    )
+
+    if(Test-Path -Path $directoryPath){
+        $directoryACL = Get-ACL -Path $directoryPath
+
+        #Disable inheritance and keep current permissions.
+        $directoryACL.SetAccessRuleProtection($True, $True)
+        
+        Set-Acl -Path $directoryPath -AclObject $directoryACL
+    }else{
+        Write-Host "Unable to reach $directoryPath."
+    }
+
+}
